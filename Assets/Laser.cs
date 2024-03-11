d2a2791caf99cd3deb06c823899e954abca2b754
Adam Biggs <wareaglebiggs@gmail.com>
2024-03-11 18:37:33 -0500

    Basic start work

---
+++ Assets/Laser.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    private void FixedUpdate()
+    {
+        transform.Rotate(0,3,0);
+    }
+}
